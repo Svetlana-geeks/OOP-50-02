059d607c52830384de29e956918055dda1d295e8
Svetlana-geeks <svetani.sn@gmail.com>
2025-01-14 00:29:15 +0600

    hw3

--- HomeWork/hw3.py
+++ HomeWork/hw3.py
@@ -17,4 +17,47 @@ class Hero:
         print(f"{self.name} защищается!")
 
     def rest(self):
+        print(f"{self.name} берет паузу")
+
+from abc import ABC, abstractmethod
+
+class Hero(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def unique_attac(self):
+        pass
+
+    @abstractmethod
+    def unique_scream(self):
+        pass
+
+    @abstractmethod
+    def action(self):
+        pass
+
+    def attack(self):
+        print(f"{self.name} атакует!")
+
+    def protection(self):
+        print(f"{self.name} защищается!")
+
+    def rest(self):
         print(f"{self.name} отдыхает!")
+
+    def action(self):
+        random_number = self.get_random_int()
+        if random_number == 1:
+            self.attack()
+        elif random_number == 2:
+            self.protection()
+        else:
+            self.rest()
+
+    class Jester(Hero):
+        def unique_attack(self):
+            print("кидает камень!")
+
+        def unique_scream(self):
+            print("смех")
